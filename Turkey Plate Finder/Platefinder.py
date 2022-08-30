2002b870a65641b8040c54a0b6759638a3508a02
Riles14R <78378589+Riles14R@users.noreply.github.com>
2022-08-30 13:51:47 +0300

    Add files via upload

---
+++ Turkey Plate Finder/Platefinder.py
@@ -0,0 +1,264 @@
+from email.mime import image
+from inspect import trace
+from time import sleep
+
+                                                                              
+
+
+Adana = "01"
+Adıyaman ="02"		
+Afyonkarahisar = "03"		
+Ağrı ="04"	
+Amasya = "05"
+Ankara = "06"
+Antalya ="07"
+Artvin ="08"	
+Aydın = "09"		
+Balıkesir ="10"	
+Bilecik ="11"		
+Bingöl="12"		
+Bitlis="13"		
+Bolu="14"
+Burdur="15"	
+Bursa= "16"		
+Çanakkale="17"		
+Çankırı="18"	
+Çorum="19"	
+Denizli="20"		
+Diyarbakır="21"
+Edirne="22"	
+Elazığ="23"	
+Erzincan = "24"	
+Erzurum ="25"	
+Eskişehir ="26"		
+Gaziantep="27"	
+Giresun="28"
+Gümüşhane="29"		
+Hakkari="30"	
+Hatay="31"	
+Isparta="32"		
+Mersin="33"	
+İstanbul="34"	
+İzmir="35"		
+Kars="36"		
+Kastamonu="37"		
+Kayseri="38"	
+Kırklareli="39"		
+Kırşehir="40"		
+Kocaeli="41"	
+Konya="42"	
+Kütahya="43"	
+Malatya="44"	
+Manisa="45"
+Kahramanmaraş="46"	
+Mardin="47"	
+Muğla="48"		
+Muş="49"		
+Nevşehir="50"		
+Niğde="51"	
+Ordu="52"	
+Rize="53"		
+Sakarya="54"	
+Samsun="55"	
+Siirt="56"		
+Sinop="57"	
+Sivas="58"		
+Tekirdağ="59"		
+Tokat="60"	
+Trabzon="61"		
+Tunceli="62"	
+Şanlıurfa="63"	
+Uşak="64"	
+Van="65"	
+Yozgat="66"		
+Zonguldak="67"		
+Aksaray="68"	
+Bayburt="69"		
+Karaman="70"
+Kırıkkale="71"		
+Batman="72"	
+Şırnak="73"	
+Bartın="74"		
+Ardahan="75"		
+Iğdır="76"	
+Yalova="77"		
+Karabük="78"		
+Kilis="79"
+Osmaniye= "80"		
+Düzce="81"	
+
+
+Platefinder=input("Which City in Turkey? ")
+delay0 = 0.5
+delay1 = 1
+sleep(delay0)
+if Platefinder == "Adana":
+ print(Adana)
+if Platefinder == "Adıyaman":
+ print(Adıyaman)
+if Platefinder == "Afyonkarahisar":
+ print(Afyonkarahisar)
+if Platefinder == "Ağrı":
+ print(Ağrı)
+if Platefinder == "Amasya":
+ print(Amasya) 
+if Platefinder == "Ankara":
+ print(Ankara)
+if Platefinder == "Antalya":
+ print(Antalya)
+if Platefinder == "Artvin":
+ print(Artvin)
+if Platefinder == "Aydın":
+ print(Aydın)
+if Platefinder == "Balıkesir":
+ print(Balıkesir) 
+if Platefinder == "Bilecik":
+ print(Bilecik)
+if Platefinder == "Bingöl":
+ print(Bingöl)
+if Platefinder == "Bitlis":
+ print(Bitlis)
+if Platefinder == "Bolu":
+ print(Bolu)
+if Platefinder == "Burdur":
+ print(Burdur) 
+if Platefinder == "Bursa":
+ print(Bursa)
+if Platefinder == "Çanakkale":
+ print(Çanakkale)
+if Platefinder == "Çankırı":
+ print(Çankırı)
+if Platefinder == "Çorum":
+ print(Çorum)
+if Platefinder == "Denizli":
+ print(Denizli)
+if Platefinder == "Diyarbakır":
+ print(Diyarbakır)
+if Platefinder == "Edirne":
+ print(Edirne)
+if Platefinder == "Elazığ":
+ print(Elazığ)
+if Platefinder == "Erzincan":
+ print(Erzincan)
+if Platefinder == "Erzurum":
+ print(Erzurum) 
+if Platefinder == "Eskişehir":
+ print(Eskişehir)
+if Platefinder == "Gaziantep":
+ print(Gaziantep)
+if Platefinder == "Giresun":
+ print(Giresun)
+if Platefinder == "Gümüşhane":
+ print(Gümüşhane)
+if Platefinder == "Hakkari":
+ print(Hakkari) 
+if Platefinder == "Hatay":
+ print(Hatay)
+if Platefinder == "Isparta":
+ print(Isparta)
+if Platefinder == "Mersin":
+ print(Mersin)
+if Platefinder == "İstanbul":
+ print(İstanbul)
+if Platefinder == "İzmir":
+ print(İzmir) 
+if Platefinder == "Kars":
+ print(Kars)
+if Platefinder == "Kastamonu":
+ print(Kastamonu)
+if Platefinder == "Kayseri":
+ print(Kayseri)
+if Platefinder == "Kırklareli":
+ print(Kırklareli)
+if Platefinder == "Kırşehir":
+ print(Kırşehir) 
+if Platefinder == "Kocaeli":
+ print(Kocaeli)
+if Platefinder == "Konya":
+ print(Konya)
+if Platefinder == "Kütahya":
+ print(Kütahya)
+if Platefinder == "Malatya":
+ print(Malatya)
+if Platefinder == "Manisa":
+ print(Manisa)
+if Platefinder == "Kahramanmaraş":
+ print(Kahramanmaraş)
+if Platefinder == "Mardin":
+ print(Mardin)
+if Platefinder == "Muğla":
+ print(Muğla)
+if Platefinder == "Muş":
+ print(Muş)
+if Platefinder == "Nevşehir":
+ print(Nevşehir) 
+if Platefinder == "Niğde":
+ print(Niğde)
+if Platefinder == "Ordu":
+ print(Ordu)
+if Platefinder == "Rize":
+ print(Rize)
+if Platefinder == "Sakarya":
+ print(Sakarya)
+if Platefinder == "Samsun":
+ print(Samsun) 
+if Platefinder == "Siirt":
+ print(Siirt)
+if Platefinder == "Sinop":
+ print(Sinop)
+if Platefinder == "Sivas":
+ print(Sivas)
+if Platefinder == "Tekirdağ":
+ print(Tekirdağ)
+if Platefinder == "Tokat":
+ print(Tokat)
+if Platefinder == "Trabzon":
+ print(Trabzon)
+if Platefinder == "Tunceli":
+ print(Tunceli)
+if Platefinder == "Şanlıurfa":
+ print(Şanlıurfa)
+if Platefinder == "Uşak":
+ print(Uşak)
+if Platefinder == "Van":
+ print(Van) 
+if Platefinder == "Yozgat":
+ print(Yozgat)
+if Platefinder == "Zonguldak":
+ print(Zonguldak)
+if Platefinder == "Aksaray":
+ print(Aksaray)
+if Platefinder == "Bayburt":
+ print(Bayburt)
+if Platefinder == "Karaman":
+ print(Karaman) 
+if Platefinder == "Kırıkkale":
+ print(Kırıkkale)
+if Platefinder == "Batman":
+ print(Batman)
+if Platefinder == "Şırnak":
+ print(Şırnak)
+if Platefinder == "Bartın":
+ print(Bartın)
+if Platefinder == "Ardahan":
+ print(Ardahan) 
+if Platefinder == "Iğdır":
+ print(Iğdır)
+if Platefinder == "Yalova":
+ print(Yalova)
+if Platefinder == "Karabük":
+ print(Karabük)
+if Platefinder == "Kilis":
+ print(Kilis)
+if Platefinder == "Osmaniye":
+ print(Osmaniye)  
+if Platefinder == "Düzce":
+ print(Düzce)
+
+
+sleep(delay1)
+
+print("Thank you for using my code,if you notice any problems contact me via discord Riles#2206")
+from PIL import Image
+with Image.open('first.png') as img:
+    img.show()
